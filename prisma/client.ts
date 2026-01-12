5891345b359c36bc602d9be04c56cfe2e16791f6
Ilyess <rachedi.ilyess@gmail.com>
2026-01-12 16:58:15 +0100

    remove rejectUnauthorized

--- prisma/client.ts
+++ prisma/client.ts
@@ -9,7 +9,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProd ? { rejectUnauthorized: false } : false,
 });
 
 export const prisma = new PrismaClient({
